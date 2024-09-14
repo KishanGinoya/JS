bea51d07d68d62cd8face04b8ff2298ea23c4558
Kishan Ginoya <138762172+KishanGinoya@users.noreply.github.com>
2024-09-14 08:42:13 +0000

    string to int conversion

--- 01_basics/03_ConversionOfDatatypes.js
+++ 01_basics/03_ConversionOfDatatypes.js
@@ -4,3 +4,10 @@ let b=String(a)//we also make it with other type also like int , boolean and oth
 console.log(typeof a)//number 
 console.log(typeof b)//string
 
+
+console.log("1"+2)//12
+console.log(1+"2")//12
+console.log("1"+2+3)//123
+console.log(1+2+"3")//33->1+2=3
+
+
