baaeacb35a4b6db4c4b8f62c5436ec78676bf57d
Kishan Ginoya <138762172+KishanGinoya@users.noreply.github.com>
2024-09-14 06:57:39 +0000

    datatypes and variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,9 @@
+const collegeName="gmca"//it can not be changed 
+let stdName="kishan ginoya"//it is used for scope
+var stdDegree="BCA"//it is basically not used because it is not include functional and variabke scope in scope area
+rollNo=1
+let stdEnr//it results undefined 
+
+console.log(collegeName)
+
+console.table([collegeName,stdName,stdDegree,rollNo,stdEnr])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+//number=>2 to power 53
+//bigint
+//string=>>"" , ''
+//boolean=>true/false
+//null=>standalone value
+//undefined=>
+//symbol=>unique
+
+
+console.log(typeof "kishan")//string
+console.log(typeof null)//object
+console.log(typeof undefined)//undefined
