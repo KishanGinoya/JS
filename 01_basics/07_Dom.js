1eb77014bc9ba46fb5042573ea829c32899f50e0
Kishan Ginoya <138762172+KishanGinoya@users.noreply.github.com>
2024-09-18 15:30:07 +0000

    dom

---
+++ 01_basics/07_Dom.js
@@ -0,0 +1,15 @@
+
+<html>
+    <head>
+        <title>DOM</title>
+    </head>
+    <body style="background-color:#212121; color:#fff">
+        <div class="Parent">
+            <div class="day">Monday</div>
+            <div class="day">Tuesday</div>
+            <div class="day">Wednesday</div>
+            <div class="day">Friday</div>
+            
+        </div>
+    </body>
+</html>
